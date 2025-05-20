afbf72e789c43152e72559e2e86cf1312980e719
Robert Malsagov <ryzone3@gmail.com>
2025-05-20 21:59:58 +0300

    койче поправил

--- scripts/index.js
+++ scripts/index.js
@@ -3,27 +3,27 @@
 const placesList = document.querySelector('.places__list');
 const template = document.querySelector('#card-template').content;
 
-function createCard (cardData, del) {
+function createCard (cardData) {
     const cardElement = template.cloneNode(true);
     const removeButton = cardElement.querySelector('.card__delete-button');
 
     cardElement.querySelector('.card__title').textContent = cardData.name;
     cardElement.querySelector('.card__image').src = cardData.link;
-    removeButton.addEventListener('click', () => deleteCardpls(del, removeButton));
+    removeButton.addEventListener('click', () => deleteCardpls(removeButton));
     
     return cardElement;
     
 }
 
-function deleteCardpls (del, element) {
+function deleteCardpls (element) {
     const removeItem = element.closest('.places__item');
     removeItem.remove()
     
 }
 
 function renderCards(cards, container) {
-    cards.forEach((card, del) => {
-        const cardRendered = createCard(card, del);
+    cards.forEach((card) => {
+        const cardRendered = createCard(card);
         container.append(cardRendered);
     });
 }
