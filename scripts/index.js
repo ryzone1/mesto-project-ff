9b869b5e205e13b87d23e86b5b83241c5c9501fa
Robert Malsagov <ryzone3@gmail.com>
2025-05-20 15:47:10 +0300

    вывел карточки на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,29 @@
 // @todo: Темплейт карточки
 
+const placesList = document.querySelector('.places__list');
+
+function createCard (cardData) {
+    const template = document.querySelector('#card-template').content; // нашел шаблон
+    const cardElement = template.cloneNode(true); //склонировал шаблон
+
+    cardElement.querySelector('.card__title').textContent = cardData.name; // в склонированном шаблоне нашел элемент .card_title и добавил текст из шаблонной карточки
+    cardElement.querySelector('.card__image').src = cardData.link; // в склонированном шаблоне нашел элемент картинку и задал атрибут срц из шаблонной карточки
+
+
+
+    return cardElement;
+}
+
+function renderCards (cards, container) {
+    cards.forEach (function(card) {
+        const cardRendered = createCard(card, null);
+        container.append(cardRendered);
+    })
+}
+
+renderCards(initialCards, placesList);
+
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
