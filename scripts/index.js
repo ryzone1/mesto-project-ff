e6b1d08e5da17fb8062912fa986acd8c1269f676
Robert Malsagov <ryzone3@gmail.com>
2025-05-20 21:58:31 +0300

    получается, закончил

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 // @todo: Темплейт карточки
 
 const placesList = document.querySelector('.places__list');
+const template = document.querySelector('#card-template').content;
 
-function createCard (cardData, deleteCard) {
-    const template = document.querySelector('#card-template').content;
+function createCard (cardData, del) {
     const cardElement = template.cloneNode(true);
     const removeButton = cardElement.querySelector('.card__delete-button');
 
     cardElement.querySelector('.card__title').textContent = cardData.name;
     cardElement.querySelector('.card__image').src = cardData.link;
-
-    deleteCard = deleteCardpls ()
-    removeButton.addEventListener('click', deleteCardpls)
-
+    removeButton.addEventListener('click', () => deleteCardpls(del, removeButton));
+    
     return cardElement;
-}
-
-function deleteCardpls () {
-    console.log('Мы кликнули по элементу');
     
 }
 
-function renderCards (cards, container) {
-    cards.forEach (function(card) {
-        const cardRendered = createCard(card);
+function deleteCardpls (del, element) {
+    const removeItem = element.closest('.places__item');
+    removeItem.remove()
+    
+}
+
+function renderCards(cards, container) {
+    cards.forEach((card, del) => {
+        const cardRendered = createCard(card, del);
         container.append(cardRendered);
-    })
+    });
 }
 
 renderCards(initialCards, placesList);
